0b8cf417cb16ce0fc06e7eb0ddc234a2d59a2e49
LAPTOP-CQAVVHJM\Lokesh K <lokeshreddy93817@gmail.com>
2024-05-12 19:31:04 +0530

    my incial commit

---
+++ src/main/java/com/example/curdApp/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.curdApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Welcome to Test Application";
+    }
+}
